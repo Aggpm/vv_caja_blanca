82a55c3869f5e803c878eda4c71aa06b9227fd52
Aggpm <a.garrudo@alumnos.upm.es>
2021-12-02 20:14:10 +0100

    PruebaSustPalabra

---
+++ src/test/java/com/practica/cajablanca/PruebaSustPalabra.java
@@ -0,0 +1,37 @@
+package com.practica.cajablanca;
+import com.cajanegra.AbstractSingleLinkedListImpl;
+import com.cajanegra.EmptyCollectionException;
+import com.cajanegra.SingleLinkedListImpl;
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.NoSuchElementException;
+import static org.junit.jupiter.api.Assertions.*;
+public class PruebaSustPalabra {
+    @Test
+    public void primero() throws EmptyCollectionException{
+        Editor e1 = new Editor();
+        Editor e2 = new Editor();
+        Editor e3 = new Editor();
+        Editor e4 = new Editor();
+        e1.leerFichero("D:\\VyV\\pruebablanca.txt");
+        e2.leerFichero("D:\\VyV\\pruebablanca2.txt");
+        e3.leerFichero("D:\\VyV\\pruebablanca3.txt");
+        e4.leerFichero("D:\\VyV\\pruebablanca4.txt");
+
+        e1.sustituirPalabra("Hola","Adios");
+        assertEquals(0, e1.size());
+
+        e2.sustituirPalabra("Hola","Adios");
+        assertEquals("[Adios]",e2.getLinea(1).toString());
+
+        e3.sustituirPalabra("Hola","Adios");
+        assertEquals("[Mario]",e3.getLinea(1).toString());
+
+        e4.sustituirPalabra("Hola","Adios");
+        assertAll("tests 5 camino",
+                ()-> assertEquals("[Adios]",e4.getLinea(1).toString()),
+                ()-> assertEquals("[Adios]",e4.getLinea(2).toString())
+        );
+    }
+}
